0e15e95606534b7a30757403556c6415678e63a5
pascalehessou@gmail.com <pascalehessou@gmail.com>
2024-12-09 04:41:14 +0100

    Ajout du fichier

---
+++ app/Http/Controllers/ChirpsController.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\View\View;
+
+class ChirpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+
+    public function index(): View
+    {
+        
+        return view('chirps.index');
+    }
+
+}
